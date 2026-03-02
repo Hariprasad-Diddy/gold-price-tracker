bc5d742e3fa731b78f7d0e269858ae95b928d91e
HariPrasad <58254570+Hariprasad-Diddy@users.noreply.github.com>
2026-03-02 21:47:28 +0530

    Update fetch_gold_price.py

--- fetch_gold_price.py
+++ fetch_gold_price.py
@@ -1,7 +1,6 @@
 from datetime import timedelta
 import requests
-import json
-import sys,os,re
+import json,sys,os,re
 import pandas as pd
 from datetime import datetime
 from zoneinfo import ZoneInfo
