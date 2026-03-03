e2318643d751ef39d4ef282a136eef1ca8145332
HariPrasad <58254570+Hariprasad-Diddy@users.noreply.github.com>
2026-03-03 22:03:51 +0530

    Reorder import statements in fetch_gold_price.py

--- fetch_gold_price.py
+++ fetch_gold_price.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
-import requests,json,sys,os,re
+import requests
+import json,sys,os,re
 import pandas as pd
 from datetime import datetime
 from zoneinfo import ZoneInfo
